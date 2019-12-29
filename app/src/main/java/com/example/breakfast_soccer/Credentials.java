409bceceff2da22c938822f5220b9ee30c7b7bcc
Cobeasta <stopyloknok@gmail.com>
2019-12-28 20:57:39 -0600

    set up the credentials in an android project

---
+++ app/src/main/java/com/example/breakfast_soccer/Credentials.java
@@ -0,0 +1,12 @@
+package com.example.breakfast_soccer;
+
+public class Credentials {
+    public static String DOC_KEY = "https://docs.google.com/spreadsheets/d/1VTcCaYQtZ6KXUcyhrY1iP9U0O923ZlPRaL2uEz6_Gqk/edit?folder=0APiSdpa2OtvjUk9PVA#gid=0";
+    public static String CLIENT_ID = "360321234779-133mfpvqmplmg4t3bqnjualkbgkbks5a.apps.googleusercontent.com";
+    public static String PROJECT_ID = "breakfast-soccer-1577386883847";
+    public static String AUTH_URI = "https://accounts.google.com/o/oauth2/auth";
+    public static String TOKEN_URI = "https://oauth2.googleapis.com/token";
+    public static String AUTH_PROVIDER = "https://www.googleapis.com/oauth2/v1/certs";
+    public static String CLIENT_SECRET = "5cgzB6lQt3yWfTULypPojQOE";
+    public static String API_KEY = "AIzaSyCcdD22sZA0-hZ5rdiURuPW2f7Fnr1WIoE";
+}
